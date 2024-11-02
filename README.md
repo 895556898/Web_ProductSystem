5a20772e507ffb197d45fa52d5c6661e873cc2cb
Shanwer <Shanwer@qq.com>
2024-11-02 23:44:08 +0800

    initial frontend

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Web_ProductSystem
 web课程设计——商品管理系统
-asd
+
+# 首次启动前端环境
+默认工作目录为项目根目录
+## 安装pnpm包管理器
+`Invoke-WebRequest https://get.pnpm.io/install.ps1 -UseBasicParsing | Invoke-Expression`
+## 安装前端依赖
+`cd frontend && pnpm install`
+
+# 启动前端
+默认工作目录为项目根目录  
+`cd frontend && pnpm start`
---
+++ frontend/config/defaultSettings.ts
@@ -0,0 +1,23 @@
+import { ProLayoutProps } from '@ant-design/pro-components';
+
+/**
+ * @name
+ */
+const Settings: ProLayoutProps & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  "title": "商品管理系统",
+  "navTheme": "light",
+  "colorPrimary": "#1890ff",
+  "layout": "mix",
+  "contentWidth": "Fluid",
+  "fixedHeader": false,
+  "fixSiderbar": true,
+  "pwa": true,
+  "logo": "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
+  "token": {},
+  "footerRender": false
+}
+
+export default Settings;
